0ff56c1d3e2e8ecafc6ac576b4f928dc717c1466
c.mejia <Cristian.A.Mejia.-ND@disney.com>
2022-08-02 20:43:03 -0500

    add jwtTokenGenerator

---
+++ BuberDinner.Application/Services/AuthenticationService.cs
@@ -0,0 +1,26 @@
+﻿using BuberDinner.Application.Common.Interfaces.Authentication;
+
+namespace BuberDinner.Application.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
+
+        public AuthenticationResult Login(string email, string password)
+        {
+            return new AuthenticationResult(Guid.NewGuid(), "", "", email, password);
+        }
+
+        public AuthenticationResult Register(string firstName, string lastName, string email, string password)
+        {
+            var userId = Guid.NewGuid();
+            var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName); 
+            return new AuthenticationResult(userId, firstName, lastName, email, token);
+        }
+    }
+}
---
+++ BuberDinner.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Infrastructure.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuberDinner.Infraestructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfraestructure(this IServiceCollection services)
+        {
+            services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+            return services;
+        }
+    }
+}
