29afe66a8925072697c9310a759ad949925511d5
c.mejia <Cristian.A.Mejia.-ND@disney.com>
2022-08-02 21:36:26 -0500

    add jwtSettings

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -5,8 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
                 .AddApplication()
-                .AddInfraestructure();
-                
+                .AddInfraestructure(builder.Configuration);
+
     builder.Services.AddControllers();
 }
 
--- BuberDinner.Infrastructure/DependencyInjection.cs
+++ BuberDinner.Infrastructure/DependencyInjection.cs
@@ -1,14 +1,19 @@
 ﻿using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Services;
 using BuberDinner.Infrastructure.Authentication;
+using BuberDinner.Infrastructure.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BuberDinner.Infraestructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfraestructure(this IServiceCollection services)
+        public static IServiceCollection AddInfraestructure(
+                                            this IServiceCollection services, Microsoft.Extensions.Configuration.ConfigurationManager configuration)
         {
+            services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
             services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+            services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
             return services;
         }
     }
